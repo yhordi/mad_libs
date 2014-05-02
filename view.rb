9b8520b513a89807f9f28564c0272d58408a286f
Tiff & Ziad Abdo <pair+TKais.souldjinn@devbootcamp.com>
2014-05-02 15:17:43 -0700

    Added new methods based on mad lib text

--- view.rb
+++ view.rb
@@ -1,30 +1,62 @@
 class View
 
-  def greeting
+  def self.greeting
     puts "Welcome to Mad Libs 1.0v!"
   end
 
-  def noun
-    puts "Give me a noun: (person, place, thing)"
+  def self.noun
+    puts "Give me a noun (person, place, thing): "
   end
 
-  def verb
-    puts "Give me a verb: (action-word) "
+  def past_noun
+    puts "Give me a noun in the past tense: "
   end
 
-  def adjective
-    puts "Give me an adjective: (descriptive-word)"
+  def self.verb
+    puts "Give me a verb in the past tense (action-word): "
   end
 
-  def adverb
+  def self.adjective
+    puts "Give me an adjective (descriptive-word): "
+  end
+
+  def self.adverb
     puts "Give me an adverb (describes an action): "
   end
 
-  def print_madlib()
+  def self.location
+    puts "Give me a wonderful location: "
+  end
+
+  def self.name
+    puts "Give me a name: "
+  end
+
+  def self.print_madlib()
 
   end
 
-  def end_game_message
+  def self.end_game_message
 
   end
 end
+
+
+#DRIVER CODE________________________________
+View.greeting
+View.noun
+View.verb
+View.adjective
+View.adverb
+View.print_madlib
+View.end_game_message
+
+story = "#{location} - in the future - is #{noun2}-ridden, and run by a #{adjective} #{noun1}.
+The #{noun1} has #{verb_past_tense} a #{adjective} #{noun2}-fighting robot, which unfortunately
+#{verb present tense} a rather #{adjective} #{noun}. The #{noun1} sees a way to get back in favor
+with the public when a cop called #{name1} is #{verb_past_tense} by a #{adjective} #{plural_noun}.
+#{name1} body is #{verb_past_tense} within a #{adjective} shell and named Robo#{name2}. Though
+Robo#{name2} is very #{adjective} against #{noun}, soon they have to face the very #{noun} who
+#{verb_past_tense} them."
+
+
