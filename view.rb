f7d73fbe7b6dff0779c021ba954aa331274032f2
Tiff & Ziad Abdo <pair+TKais.souldjinn@devbootcamp.com>
2014-05-02 14:11:07 -0700

    *added greeting method  *added prompting messages   *noun   *verb   *adjective   *adverb *added skeleton method for printing finished madlib *added skeleton method for an endgame message

---
+++ view.rb
@@ -0,0 +1,30 @@
+class View
+
+  def greeting
+    puts "Welcome to Mad Libs 1.0v!"
+  end
+
+  def noun
+    puts "Give me a noun: (person, place, thing)"
+  end
+
+  def verb
+    puts "Give me a verb: (action-word) "
+  end
+
+  def adjective
+    puts "Give me an adjective: (descriptive-word)"
+  end
+
+  def adverb
+    puts "Give me an adverb (describes an action): "
+  end
+
+  def print_madlib()
+
+  end
+
+  def end_game_message
+
+  end
+end
