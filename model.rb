87a7a61de1d5ede96ab09f62decce25dbd887946
Joshua Rosaaen & Jordan Kamin <pair+jrosaaen.yhordi@devbootcamp.com>
2014-05-02 14:32:38 -0700

    interpolated text

--- model.rb
+++ model.rb
@@ -4,8 +4,9 @@
 # I18n.enforce_available_locals = false
 
 # Generate SQL database method
+#Detroit - in the future - is crime-ridden, and run by a massive company. The company have developed a huge crime-fighting robot, which unfortunately develops a rather dangerous glitch. The company sees a way to get back in favor with the public when a cop called Alex Murphy is killed by a violent robbers. Murphy's body is reconstructed within a steel shell and named RoboCop. Though RoboCop is very successful against criminals, soon he has to face the very gang who killed him.
 
-story = "#{Location} - in the future - is #{noun2}-ridden, and run by a #{adjective} #{noun1}. The #{noun1} have #{verb past-tense} a #{adjective} #{noun2}-fighting robot, which #{adverb} #{verb present tense} a rather #{adjective} #{noun}. The #{noun1} sees a way to get back in favor with the #{noun} when a cop called #{name1} is #{verb past-tense} by a #{adjective} #{plural noun}. #{name1} body is #{verb past-tense} within a #{adjective} shell and named Robo#{name2}. Though Robo#{name2} is very #{adjective} against #{noun}, soon they have to face the very #{noun} who #{verb past-tense} them."
+story = "#{location} - in the future - is #{noun2}-ridden, and run by a #{adjective} #{noun1}. The #{noun1} has #{verb_past_tense} a #{adjective} #{noun2}-fighting robot, which unfortunately #{verb present tense} a rather #{adjective} #{noun}. The #{noun1} sees a way to get back in favor with the public when a cop called #{name1} is #{verb_past_tense} by a #{adjective} #{plural_noun}. #{name1} body is #{verb_past_tense} within a #{adjective} shell and named Robo#{name2}. Though Robo#{name2} is very #{adjective} against #{noun}, soon they have to face the very #{noun} who #{verb_past_tense} them."
 
 
 
