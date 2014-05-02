e6ae5555fa2a7b5704f1ed92184438764b7c75a6
Joshua Rosaaen & Jordan Kamin <pair+jrosaaen.yhordi@devbootcamp.com>
2014-05-02 14:17:01 -0700

    text file

--- model.rb
+++ model.rb
@@ -1,42 +1,51 @@
-DATABASE_NAME = 'madlibs'
-require 'sqlite'
-db_connection = SQLite3::Database.new"#{DATABASE_NAME}.db"
-I18n.enforce_available_locals = false
+# DATABASE_NAME = 'madlibs'
+# require 'sqlite'
+# db_connection = SQLite3::Database.new"##{DATABASE_NAME}.db"
+# I18n.enforce_available_locals = false
 
 # Generate SQL database method
 
-# db clear table for libs
+story = "#{Location} - in the future - is #{noun2}-ridden, and run by a #{adjective} #{noun1}. The #{noun1} have #{verb past-tense} a #{adjective} #{noun2}-fighting robot, which #{adverb} #{verb present tense} a rather #{adjective} #{noun}. The #{noun1} sees a way to get back in favor with the #{noun} when a cop called #{name1} is #{verb past-tense} by a #{adjective} #{plural noun}. #{name1} body is #{verb past-tense} within a #{adjective} shell and named Robo#{name2}. Though Robo#{name2} is very #{adjective} against #{noun}, soon they have to face the very #{noun} who #{verb past-tense} them."
+
+
+
+
 class MadLib
 
-  def noun
-  end
-
-  def noun_plural
-  end
-
-  def pronoun
-  end
-
-  def location
-  end
-
-  def name
-  end
-
-  def verb
+  def self.noun
 
   end
 
-  def verb_past_tense
+  def self.noun_plural
+
   end
 
-  def verb_present_tense
+  def self.pronoun
+
   end
 
-  def adjective
+  def self.location
+
   end
 
-  def adverb
+  def self.name
+
+  end
+
+  def self.verb
+
+  end
+
+  def self.verb_past_tense
+  end
+
+  def self.verb_present_tense
+  end
+
+  def self.adjective
+  end
+
+  def self.adverb
 
   end
 
