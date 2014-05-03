0345fea2666c829ca56af62492f1cc5d4160a9eb
Tommy <null>
2014-05-02 19:14:45 -0700

    Script that when run will create a database. Only needs to be run once

---
+++ dbcreator.rb
@@ -0,0 +1,18 @@
+#this file should only be run ONCE when you
+# want to create a database or drop an exisiting one.
+
+DATABASE_NAME='mad_libs_database'
+require 'sqlite3'
+
+db_connection= SQLite3::Database.new "#{DATABASE_NAME}.db"
+
+db_connection.execute("DROP TABLE IF EXISTS stories;")
+
+db_connection.execute(<<-SQL
+    CREATE TABLE stories (
+    id INTEGER DEFAULT NULL PRIMARY KEY AUTOINCREMENT,
+    content BLOB DEFAULT NULL,
+    time_created TIMESTAMP DEFAULT NULL
+    );
+    SQL
+    )
